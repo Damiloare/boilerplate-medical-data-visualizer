476132fbb3f4fc81341cfc0d635535674afade91
Damiloare <162570949+Damiloare@users.noreply.github.com>
2024-07-09 20:26:36 +0100

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df["overweight"] = np.where(df["weight"] / (df["height"] * df["height"]) * 10000 > 25,1,0)
 
 # 3
-
-
+df['cholesteral'] = np.where(df['cholesterol'] == 1, 0, 1)
+df['gluc'] = np.where(df['gluc'] == 1, 0, 1)
 # 4
 def draw_cat_plot():
     # 5
@@ -38,8 +38,10 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
-
+    df_heat = (df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) &
+    (df['height'] <= df['height'].quantile(0.975)) &
+    (df['weight'] >= df['weight'].quantile(0.025)) &
+    (df['weight'] <= df['weight'].quantile(0.975))]
     # 12
     corr = None
 
